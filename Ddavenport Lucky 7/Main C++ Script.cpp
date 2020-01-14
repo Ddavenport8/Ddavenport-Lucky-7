3a13a1b71d882bf188cd0971aa0c676ce830047e
Daniel Davenport <pinerd10@gmail.com>
2020-01-14 10:09:02 -0700

    Created Part 2

--- Ddavenport Lucky 7/Main C++ Script.cpp
+++ Ddavenport Lucky 7/Main C++ Script.cpp
@@ -2,10 +2,12 @@
 //This Code is my own
 #include <iostream>
 using namespace std;
-int smallest, largest, num1, num2, num3, num4, num5;
 
+
+/*
 int main()
 {
+	int smallest, largest, num1, num2, num3, num4, num5;
 	cout << "Enter 5 different integers to see which is the largest and smallest \n";
 	cin >> num1 >> num2 >> num3 >> num4 >> num5;
 	cout << "Your numbers are " << num1 << " " << num2 << " " << num3 << " " << num4 << " and " << num5;
@@ -22,4 +24,20 @@ int main()
 	if (num5 < smallest) smallest = num5;
 
 	cout << "\nYour largest number is " << largest << " \nYour smallest number is " << smallest;
+}
+*/
+
+int main() 
+{
+	//part 2 Write a program that calculates and prints
+	//the sum of the first 50 positive integers that are multiples of 7.
+	int i = 1;
+	int sum = 0;
+	while (i <= 50)
+	{
+		sum = sum + i * 7;
+		i++;
+	}
+
+	cout << "The Sum of the first 50 positive multiples of 7 is " << sum << endl;
 }
