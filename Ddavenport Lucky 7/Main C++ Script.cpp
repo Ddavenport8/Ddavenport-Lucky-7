0e2b60617dde20b8464793712fbc6d1ba20be776
Daniel Davenport <pinerd10@gmail.com>
2020-01-26 21:46:41 -0700

    Part 7 is Done, changed all code to be called in one instance

--- Ddavenport Lucky 7/Main C++ Script.cpp
+++ Ddavenport Lucky 7/Main C++ Script.cpp
@@ -2,23 +2,23 @@
 //This Code is my own
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 
-/*
-int main()
+void main1()
 {
 	int smallest, largest, num1, num2, num3, num4, num5;
 	cout << "Enter 5 different integers to see which is the largest and smallest \n";
 	cin >> num1 >> num2 >> num3 >> num4 >> num5;
 	cout << "Your numbers are " << num1 << " " << num2 << " " << num3 << " " << num4 << " and " << num5;
-	int largest = num1;
+	largest = num1;
 	if (num2 > largest) largest = num2;
 	if (num3 > largest) largest = num3;
 	if (num4 > largest) largest = num4;
 	if (num5 > largest) largest = num5;
 
-	int smallest = num1;
+	smallest = num1;
 	if (num2 < smallest) smallest = num2;
 	if (num3 < smallest) smallest = num3;
 	if (num4 < smallest) smallest = num4;
@@ -28,7 +28,7 @@ int main()
 }
 
 
-int main() 
+void main2() 
 {
 	//part 2 Write a program that calculates and prints
 	//the sum of the first 50 positive integers that are multiples of 7.
@@ -43,7 +43,7 @@ int main()
 	cout << "The Sum of the first 50 positive multiples of 7 is " << sum << endl;
 }
 
-int main()
+void main3()
 {
 	//Part 3
 	//Calculates and prints the first 10 terms of the factorial sequence.
@@ -62,7 +62,7 @@ int main()
 }
 
 
-int main()
+void main4()
 {
 	int x, i;
 	bool tester = false;
@@ -86,12 +86,12 @@ int main()
 	else {
 		cout << input << " is a palindrome\n";
 	}
-	return 0;
+	
 	
 }
 
 
-int main()
+void main5()
 {
 
 	bool isPrime = false;
@@ -99,8 +99,8 @@ int main()
 	int input;
 	cout << "Input a positive integer to see if it is a prime number: \n";
 	cin >> input;
+
 	
-	int x = input / 2;
 	if (input < 0)
 	{
 		cout << "That is not a valid input";
@@ -109,7 +109,7 @@ int main()
 	{
 
 
-		for (int i = 2; i <= x; i++)
+		for (int i = 2; i <= input; i++)
 		{
 			if (input % i == 0)
 			{
@@ -132,9 +132,10 @@ int main()
 		cout << input << final << "a prime number \n";
 
 	}
-	*/
+}
+	
 
-int main()
+void main6()
 {
 	int list [5] = {};
 	int i, numofint = 0;
@@ -150,3 +151,75 @@ int main()
 
 	cout << "The average of those numbers is " << sum / numofint;
 }
+
+void main7()
+{	
+	int n;
+	int longest = 0, ct = 0;
+	ifstream fin("input.in");
+	cout << "Enter an integer to select that many words from a text document, the longest word will then be dertermined: \n";
+	cin >> n;
+	string s;
+	string *stringarray= new string[n];
+	while (ct < n)
+	{
+		fin >> stringarray[ct];
+		ct++;
+	}
+	for (int x = 0; x < n;x++)
+	{
+			if (stringarray[x].size() > stringarray[longest].size())
+			{
+				longest = x;
+			}
+	}
+	cout << "The longest word is: \n" << stringarray[longest];
+	if (!fin) 
+	{
+		cerr << "File not found" << endl;
+		exit(2);
+	}
+	//while (fin >> s)
+	//	cout << s << endl;
+
+	
+	}
+int main()
+{
+	int choice;
+	cout << "Choose which 'Lucky Seven' sections to run between 1 and 7 \n";
+	cin >> choice;
+	if (choice == 1)
+	{
+		main1();
+	}
+	else if (choice == 2)
+	{
+		main2();
+	}
+	else if (choice == 3)
+	{
+		main3();
+	}
+	else if (choice == 4)
+	{
+		main4();
+	}
+	else if (choice == 5)
+	{
+		main5();
+	}
+	else if (choice == 6)
+	{
+		main6();
+	}
+	else if (choice == 7)
+	{
+		main7();
+	}
+	else
+	{
+		cout << "That is not a valid input";
+	}
+}
+
